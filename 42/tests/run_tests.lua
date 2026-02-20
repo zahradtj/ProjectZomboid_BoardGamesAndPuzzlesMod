fc2b21a42937cb6b6418bad6ec90b8d75142efb2
zahradtj <trey.j.zahradka@gmail.com>
2026-02-19 21:49:51 -0600

    try fixing tests again

--- 42/tests/run_tests.lua
+++ 42/tests/run_tests.lua
@@ -28,13 +28,12 @@ end
 
 reset_test_globals()
 
+add_path(root)
 
 -- PZ loads from media/lua/(shared|client|server)
 add_path(root .. "/media/lua/shared")
 add_path(root .. "/media/lua/client")
 add_path(root .. "/media/lua/server")
-
-add_path(root .. "/tests")
 
 -- LuaUnit vendored here
 add_path(root .. "/tests/libs")
